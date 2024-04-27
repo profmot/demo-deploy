0e3ea95b6342cde705905613efa4ebd92a71e550
profmot <mo.tremblay@cmontmorency.qc.ca>
2024-04-27 12:06:42 -0400

    MOT: Correctifs + ajustement code de base frontend et backend

--- backend/index.js
+++ backend/index.js
@@ -5,13 +5,23 @@ const messageRoutes = require('./routes/messages-routes');
 
 const app = express();
 // chercher les variables d'environnemnt
-const PORT = process.env.PORT || 3000;
-const MONGODB_URI =
-  process.env.MONGODB_URI || 'mongodb://localhost:27017/db-deploy';
+const PORT = 5000;
+const MONGODB_URI = 'mongodb://localhost:27017/db-deploy';
 
 // section des middlewares
 
 app.use(express.json());
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*'); // header et value * quels domaines peuvent acceder a notre serveur
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  ); //quel header sont autorisés ( pourait etre * pour tout)
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE'); // quelles methodes HTTP sont autorisées
+  next();
+});
+
 app.use('/api/messages', messageRoutes);
 
 //connexion BD + demarrage serveur web
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,9 +10,9 @@ const App = () => {
 
   const fetchData = async () => {
     try {
-      const response = await fetch(process.env.REACT_APP_BACKEND_URL);
+      const response = await fetch('http://localhost:5000/api/messages/');
       const jsonData = await response.json();
-      setData(jsonData);
+      setData(jsonData.messages);
     } catch (error) {
       console.error('Error fetching data:', error);
     }
@@ -20,15 +20,18 @@ const App = () => {
 
   const postData = async () => {
     try {
-      const response = await fetch(process.env.REACT_APP_BACKEND_URL, {
+      const response = await fetch('http://localhost:5000/api/messages/', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
         },
-        body: JSON.stringify({ message: 'Hello, backend!' }),
+        body: JSON.stringify({
+          message: 'Bonjour à tous! : ' + new Date().toLocaleString(),
+        }),
       });
       const jsonData = await response.json();
       console.log('Response from backend:', jsonData);
+      fetchData();
     } catch (error) {
       console.error('Error posting data:', error);
     }
@@ -40,7 +43,7 @@ const App = () => {
       <button onClick={postData}>Send POST Request</button>
       <ul>
         {data.map((item) => (
-          <li key={item.id}>{item.name}</li>
+          <li key={item.id}>{item.message}</li>
         ))}
       </ul>
     </div>
